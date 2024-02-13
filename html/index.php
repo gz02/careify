e3261feeef6ccfe5557ddabc942d6c7b83bd4ae7
gz02 <g.zawadzki@wlv.ac.uk>
2024-02-13 01:45:47 +0000

    removed unnecessary header

--- html/index.php
+++ html/index.php
@@ -1,6 +1,5 @@
 <?php
 
-header("HTTP/1.1 202 Accepted");
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, proxy-revalidate, must-revalidate");
