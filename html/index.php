64ae279d80f09fb68f9713f97e2ef8c60a586fec
gz02 <g.zawadzki@wlv.ac.uk>
2024-03-12 06:58:48 +0000

    fixed nocache header mess

--- html/index.php
+++ html/index.php
@@ -1,10 +1,6 @@
 <?php
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, proxy-revalidate, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Cache-Control: max-age=10000000, s-maxage=1000000");
+header("Cache-Control: no-store");
 header("Pragma: no-cache");
 
 require_once("/var/www/private/lib/vendor/autoload.php");
