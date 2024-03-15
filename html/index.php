3c8f1792016bf79e972747ae4f205588c34b961e
gz02 <g.zawadzki@wlv.ac.uk>
2024-03-15 17:18:09 +0000

    better page routing

--- html/index.php
+++ html/index.php
@@ -6,36 +6,37 @@ header("Pragma: no-cache");
 require_once("/var/www/private/session.php");
 require_once("/var/www/private/lib/vendor/autoload.php");
 
-session_start();
+session_start(); // check for login
 
 $twig = new \Twig\Environment(new \Twig\Loader\FilesystemLoader(getcwd()));
 
-if ($_SERVER["REQUEST_METHOD"] === "GET")
+// pages allowed for everyone
+if (isset($_GET["login"])) { echo $twig->render("login.html"); }
+else if (isset($_GET["user-login"])) { echo $twig->render("user-login.html"); }
+else if (isset($_GET["carer-login"])) { echo $twig->render("carer-login.html"); }
+else if (isset($_GET["signup"])) { echo $twig->render("signup.html"); }
+
+else if (isset($_SESSION["elderly_id"])) // pages requiring elderly login
 {
-	// pages allowed for everyone
-	if (isset($_GET["login"])) { echo $twig->render("login.html"); exit; }
-	if (isset($_GET["user-login"])) { echo $twig->render("user-login.html"); exit; }
-	if (isset($_GET["carer-login"])) { echo $twig->render("carer-login.html"); exit; }
-	if (isset($_GET["signup"])) { echo $twig->render("signup.html"); exit; }
-	
-	// pages requiring carer login
-	if (1)
+	if (isset($_GET["todo"])) { echo $twig->render("todo.html"); }
+	else if (isset($_GET["mood"])) { echo $twig->render("mood.html"); }
+	else if (isset($_GET["profile"])) { echo "Not complete.";/*$twig->render("profile.html");*/ }
+	else if (isset($_GET["reminder"])) { echo "Not complete.";/*$twig->render("reminder.html");*/ }
+	else // default to interface
 	{
-		if (isset($_GET["carer-interface"])) { echo $twig->render("carer-interface.html"); exit; }
-	}
-	
-	// pages requiring elderly login
-	if (isset($_SESSION["elderly_id"]))
-	{
-		if (isset($_GET["user-interface"])) { echo $twig->render("user-interface.html", ["moodImg" => $_GET["mood"] ?? ""]); exit; }
-		if (isset($_GET["todo"])) { echo $twig->render("todo.html"); exit; }
-		if (isset($_GET["mood"])) { echo $twig->render("mood.html"); exit; }
-		if (isset($_GET["profile"])) { echo "Not complete.";/*$twig->render("profile.html");*/ exit; }
-		if (isset($_GET["reminder"])) { echo "Not complete.";/*$twig->render("reminder.html");*/ exit; }
+		echo $twig->render("user-interface.html", ["moodImg" => $_GET["mood"] ?? ""]);
 	}
 }
 
-echo $twig->render("desktop.html");
+else if (0) // pages requiring carer login
+{
+	if (0) { echo $twig->render(""); }
+	else // default to interface
+	{
+		echo $twig->render("carer-interface.html");
+	}
+}
+else { echo $twig->render("desktop.html"); } // always respond with index
 
-exit;
+http_response_code(200); exit;
 ?>
