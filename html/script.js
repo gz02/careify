02d36bb3f679ce7bd10a420eb6582ee4c4709c0e
sbhanushali2010 <sbhanushali2010@gmail.com>
2024-03-15 00:02:06 +0000

    jscript for current mood update on user-interface

--- html/script.js
+++ html/script.js
@@ -19,6 +19,22 @@ const backButton = document.querySelector('.backbutton');
 backButton.addEventListener('click', () => {
 window.location.href = 'user-interface.html';
 });
+//Mood Update Feature
+const happy = document.getElementById('happy');
+const fine = document.getElementById('fine');
+const sad = document.getElementById('sad');
+
+happy.addEventListener('click', () => {
+    window.location.href = 'user-interface.html?mood=happy.png';
+});
+
+fine.addEventListener('click', () => {
+    window.location.href = 'user-interface.html?mood=fine.png';
+});
+
+sad.addEventListener('click', () => {
+    window.location.href = 'user-interface.html?mood=sad.png';
+});
 
 function submitForm() {
   // data collected for each element.
