f5790deec9cc73e899f0b927a70fe2ee0622b312
Leigh Clarke <leigh@physiqgroup.com>
2024-05-06 16:12:27 +0100

    carer-interface

--- html/carer-interface.js
+++ html/carer-interface.js
@@ -1,4 +1,4 @@
-function loadMoods() {
+/*function loadMoods() {
 	fetch("/api?all-mood")
 	.then(ret => ret.text())
 	.then(ret => { document.getElementById("usersMood").innerHTML = ret; });
@@ -6,4 +6,23 @@ function loadMoods() {
 
 document.addEventListener("DOMContentLoaded", function () {
 	loadMoods();
-});
+}); */
+
+var modal = document.getElementById("addTaskModal");
+var addTaskBtn = document.querySelector(".button.morebutton");
+var closeBtn = document.querySelector(".closebutton");
+
+addTaskBtn.onclick = function() {
+	console.log("hello");
+    modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
